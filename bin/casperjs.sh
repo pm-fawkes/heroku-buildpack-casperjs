5544e63519e901709d45227e2ad17d2399741382
orlando lee <olee@wexusapp.com>
2016-08-19 13:24:04 -0700

    Update casperjs.sh
    fix casperjs can not be found issue on heroku

--- bin/casperjs.sh
+++ bin/casperjs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Add phantomjs/casperjs paths ..."
-PATH="${PATH}:/app/vendor/phantomjs/bin:/app/vendor/casperjs/bin"
+PATH="/app/vendor/phantomjs/bin:/app/vendor/casperjs/bin:${PATH}"
 LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/app/vendor/phantomjs/lib"
 
